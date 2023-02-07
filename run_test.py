872264d43419662518b66b7e4e606c32410a00e0
E-NoR <enorenor@gmail.com>
2023-02-07 17:38:26 +0800

    adjust 調整列印架構

--- run_test.py
+++ run_test.py
@@ -2,27 +2,22 @@
 # @Author: E-NoR
 # @Date:   2023-01-19 18:28:43
 # @Last Modified by:   E-NoR
-# @Last Modified time: 2023-02-07 16:22:25
+# @Last Modified time: 2023-02-07 17:38:08
 from os import listdir, path, rename, system
 from re import findall
-from subprocess import PIPE, run
+from subprocess import PIPE, Popen, run
 from time import localtime, strftime
+
 from loguru import logger
 from lxml.etree import HTML
 
+
 @logger.catch
-def rename_report(result):
-    try:
-        file_path = findall(r'\\\\(.*)"', result.stderr.decode().splitlines()[-2])[-1]
-    except IndexError as e:
-        for res in result.stderr.decode().splitlines()[-10:]:
-            print(res)
-        raise IndentationError(res) from e
-    # file_path = sorted(listdir(REPORT_PATH),key=lambda x: path.getmtime(path.join(REPORT_PATH, x)))[-1]
-    timestamp_file = file_path[7:-5]
+def rename_report(file_path):
+    timestamp_file = findall('-(.*)\.',file_path)[0]
     timestamp_srft = strftime("%Y-%m-%d_%H-%M-%S", localtime(int(timestamp_file)))
-    newpath = f"{REPORT_PATH}{file_path.replace(timestamp_file,timestamp_srft)}"
-    rename(f"{REPORT_PATH}{file_path}", newpath)
+    newpath = f"{file_path.replace(timestamp_file,timestamp_srft)}"
+    rename(f"{file_path}", newpath)
     print("outputpath : ", newpath)
     return newpath
 
@@ -36,23 +31,24 @@ def print_result(file_path):
 
 
 def run_test(target_list):
-    # file_list = set()
-
-    # for target in target_list:
-    #     if target.split(".")[0] != "":
-    #         file_list.update({path.join(target, file) for file in listdir(target) if file.endswith(".json")})
-    #     if str(target).endswith(".json"):
-    #         file_list.add(target)
-    # file_params = " ".join(file_list)
     file_params = " ".join(target_list)
     cmd = f".\\hrp.exe run {file_params} -g -c --venv ./pyenv/"
-    result = run(cmd, stdout=PIPE, stderr=PIPE)  # ,capture_output=True
-    print_result(rename_report(result))
-    # system(cmd)
+    process  = Popen(cmd, stdout=PIPE,stderr=PIPE)
 
+    file_path = None
+    while True:
+        output = process.stderr.readline()
+        if output == b'' and process.poll() is not None:
+            break
+        if output:
+            msg = output.strip().decode()
+            color = '\033[91m[ERR]' if 'err' in msg else '\033[32m[INF]'
+            print(f'{color}\033[0m {msg}')
+            if 'generate HTML report path' in msg:
+                file_path = findall('\"(.*)\"',msg)[0]
+                
 
-REPORT_PATH = "./reports/"
+    print_result(rename_report(file_path))
 
-file_list = ["testcases\YL_SIT_case"]
-# file_list = ["testcases\YL_SIT_case\YL_SIT_001_運營管理\YL_SIT_001_運營管理_IP白名單_開啟子板塊_test.json"]
+file_list = ["testcases_base\YL_SIT_001_運營管理_IP白名單_開啟子板塊_test.json"]
 run_test(file_list)
